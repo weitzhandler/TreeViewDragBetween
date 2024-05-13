11654153136e3bae3124704391bd874a0706d7da
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2024-05-13 01:12:00 +0100

    refactor: add data

--- TreeViewDragBetween/MainPage.xaml.cs
+++ TreeViewDragBetween/MainPage.xaml.cs
@@ -4,12 +4,10 @@ public sealed partial class MainPage : Page
 {
     public MainPage()
     {
-        this.InitializeComponent();
-
-
+        this.InitializeComponent();        
     }
 
-    public Node Root =>
+    public IList<Node> Items => [
         new Node(1, [
             new Node(2, [
                 new Node(3),
@@ -23,13 +21,35 @@ public sealed partial class MainPage : Page
                 new Node(10),
                 new Node(11),
                 ])
-            ]);
+            ]),
+        new Node(12, [
+            new Node(13, [
+                new Node(14),
+                new Node(15),
+                new Node(16),
+                ]),
+            new Node(17, [
+                new Node(18),
+                new Node(19),
+                new Node(20),
+                new Node(21),
+                ])
+            ])
+
+        ];
 
 }
 
-public record Node(int Number, IList<Node> Children)
+public class Node(int number, IList<Node> children)
 {
-    public Node(int Number) : this(Number, [])
-    {
+    private Node() : this(default)
+    {            
     }
+
+    public Node(int number) : this(number, [])
+    {            
+    }
+
+    public int Number => number;
+    public IList<Node> Children => children;
 }
